a3f98d0e52e071d1273713963a48d693c39d3653
Sunairaa <sunairashahzeb@gmail.com>
2022-08-23 10:52:43 +0200

    get response from food api on front end

--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,23 @@ document.addEventListener(
   },
   false
 );
+const apiKey = '6htf03n46hsW3piW88qt8gDIpAha0ewMtWfshMqC';
+const ingredients = document.getElementById('inputIngredients');
+const ingredientsList = document.getElementById('datalistOptions');
+let option;
+ingredients.addEventListener('input', () => {
+  axios
+    .get(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${apiKey}&query=${ingredients.value}&pageSize=5`)
+    .then(response => {
+      ingredientsList.innerHTML = '';
+      console.log('Response from API is: ', response.data.foods);
+      for(let i = 0; i < response.data.foods.length; i++) {
+        option = document.createElement('option');
+        option.setAttribute('value', response.data.foods[i].fdcId);
+        option.textContent = response.data.foods[i].description;
+        ingredientsList.appendChild(option);
+      }
+    })
+    .catch(err => console.log(err));
+  // alert("hello");
+})
