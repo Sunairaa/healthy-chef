61affddca1b7825a0f16432d429fdd324a6edce4
Sunairaa <sunairashahzeb@gmail.com>
2022-08-26 11:27:32 +0200

    updated ingredient data

--- seed/seed.js
+++ seed/seed.js
@@ -1,10 +1,15 @@
 const Ingredient = require('../models/Ingredient.model');
-const data = require('../routes/ingredients-data.json');
+const data = require('./ingredients-data.json');
+const mongoose = require('mongoose');
+
 // ℹ️ Connects to the database
 require("../db");
 
 Ingredient.deleteMany()
-    .then(Ingredient.insertMany(data))
+    .then(() => Ingredient.insertMany(data))
     .then((result) => {
-        console.log("Seeded data:" + result)
+        console.log("Seeded data:" + JSON.stringify(result))
+        mongoose.connection.close()
     })
+    .catch((err) => console.error(err))
+   
